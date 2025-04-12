a0e9ed3228e936a64670e1e882e8368ba9bb4904
Zeay <zeayycool@gmail.com>
2025-04-12 17:30:22 +0530

    cleanup

--- src/main.ts
+++ src/main.ts
@@ -1,11 +1,12 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
 import * as cors from 'cors';
+const port = process.env.PORT || 4000;
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
   app.use(cors());
-  await app.listen(process.env.PORT ?? 3001);
+  await app.listen(port ?? 3001);
   console.log('Server running on http://localhost:3001');
 }
 bootstrap();
